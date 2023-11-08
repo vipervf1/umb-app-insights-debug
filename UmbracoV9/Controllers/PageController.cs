167d9e249e99a7705dc8f4b685bbc874025b96b4
Alex Vilmur <alex@vilmurnet.com>
2023-11-08 07:44:17 -0600

    Added v9 project

---
+++ UmbracoV9/Controllers/PageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using System;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoV9.Controllers {
+    public class PageController : RenderController {
+        private readonly ILogger<RenderController> _logger;
+
+        public PageController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor) {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        public override IActionResult Index() {
+            _logger.LogInformation("Someone Viewed the Index");
+
+            if (Request.Query.ContainsKey("warn")) {
+                _logger.LogWarning("Someone found the warning inducer.");
+            }
+
+            if (Request.Query.ContainsKey("error")) {
+                throw new Exception("Someone found the error inducer.");
+            }
+
+
+            return base.Index();
+        }
+    }
+}
