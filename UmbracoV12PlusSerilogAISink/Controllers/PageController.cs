f1d9d1441a7c48552ad3d472f35160e4b56e4834
Alex Vilmur <alex@vilmurnet.com>
2023-11-22 13:00:26 -0600

    Added document type

---
+++ UmbracoV12PlusSerilogAISink/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoV12PlusSerilogAISink.Controllers {
+    public class PageController : RenderController {
+        private readonly ILogger<RenderController> _logger;
+
+        public PageController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor) {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override IActionResult Index() {
+            _logger.LogInformation("Someone Viewed the Index");
+
+            if (Request.Query.ContainsKey("warn")) {
+                _logger.LogWarning("Someone found the warning inducer.");
+            }
+
+            if (Request.Query.ContainsKey("error")) {
+                throw new Exception("Someone found the error inducer.");
+            }
+
+
+            return base.Index();
+        }
+    }
+}
